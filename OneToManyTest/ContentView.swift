baffb82650a0a9d8f9a1276a889c2a0272f88ac0
Tony Miller <tonylmiller@mac.com>
2020-03-26 17:02:00 -0400

    Initial Commit

---
+++ OneToManyTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OneToManyTest
+//
+//  Created by Tony Miller on 3/26/20.
+//  Copyright © 2020 Fahrsoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
